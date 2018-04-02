15cd8927f315cead934d976cb749cb5b2108ad47
Konstantinos Chatzis <kostaschatzis01@gmail.com>
2018-04-02 18:45:18 +0300

    Update travis-build.sh

--- travis-build.sh
+++ travis-build.sh
@@ -2,4 +2,4 @@
 apt install -y git subversion gcc g++ binutils patch bzip2 flex make gettext pkg-config unzip libz-dev libncurses-dev gawk gcc-multilib
 ./scripts/feeds update -a
 ./scripts/feeds install -a
-make -j1 V=99
+make -j4 V=99
